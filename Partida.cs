be955ab8eb9f18fd84f59cdf15e2f198e2c95740
Welb Cesar <welb.junior@alunos.unis.edu.br>
2020-04-12 22:05:50 -0300

    first commit

---
+++ Partida.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ProjFutebol_01
+{
+    class Partida
+    {
+        //Atributos
+        private DateTime data;
+        private Time timeCasa;
+        private Time timeVisitante;
+        private int placarCasa;
+        private int placarVisitante;
+
+        //Construtor
+        public Partida(DateTime dataPartida, Time Tcasa, Time Tvisitante)
+        {
+            this.data = dataPartida;
+            this.timeCasa = Tcasa;
+            this.timeVisitante = Tvisitante;
+            this.placarCasa = 0;
+            this.placarVisitante = 0;
+        }
+
+        //Metodos
+        public int golCasa()
+        {
+            return placarCasa++;
+        }
+
+        public int golVisitante()
+        {
+            return placarVisitante++;
+        }
+
+        public string getPalacar()
+        {
+            if (this.placarCasa > this.placarVisitante)
+            {
+                this.timeCasa.Vitorias = 1;
+                this.timeVisitante.Derrotas = 1;
+                return "O vencedor da partida em " + this.data.ToString("dd-MM-yyyy") +
+                    " entre " + this.timeCasa.Nome +
+                    " x " + this.timeVisitante.Nome +
+                    " foi o time " + this.timeCasa.Nome +
+                    " com o placar de " + this.placarCasa.ToString() +
+                    " a " + this.placarVisitante.ToString() + ".";
+            }
+
+            if (this.placarCasa == this.placarVisitante)
+            {
+                this.timeCasa.Empates = 1;
+                this.timeVisitante.Empates = 1;
+                return "Sem vencedor na partida em " + this.data.ToString("dd-MM-yyyy") +
+                    " com o placar de " + this.placarCasa + " a " + this.placarVisitante;
+            }
+
+            this.timeVisitante.Vitorias = 1;
+            this.timeCasa.Derrotas = 1;
+            return "O vencedor da partida em " + this.data.ToString("dd-MM-yyyy") +
+                " entre " + this.timeVisitante.Nome + " x " + this.timeCasa.Nome +
+                " foi o time " + this.timeVisitante.Nome +
+                " com o placar de " + this.placarVisitante + " a " + this.placarCasa;
+        }
+
+        public void jogar()
+        {
+            for(int i=0 ; i<9; i++ )
+                {
+                    golCasa();
+                }
+            golVisitante();
+            
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjFutebol_01
+{
+    class Time
+    {
+        //atributos
+        private string nome;
+        private Int32 vitorias;
+        private Int32 derrotas;
+        private Int32 empates;
+        private List<Jogador> jogadores;
+
+        //construtor
+        public Time(string Nome)
+        {
+            this.nome = Nome;
+            this.vitorias = 0;
+            this.derrotas = 0;
+            this.empates = 0;
+            this.jogadores = new List<Jogador>();
+        }
+
+        //metodos
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        public string getStatus()
+        {
+            return "O status do Time " + this.Nome +
+                    " é Vitorias:" + this.Vitorias +
+                    "\tDerrotas: " + this.derrotas +
+                    "\tEmpates: " + this.empates;
+        }
+
+        public Int32 Vitorias
+        {
+            get { return vitorias; }
+            set { vitorias += value; }
+        }
+
+        public Int32 Derrotas
+        {
+            get { return derrotas; }
+            set { derrotas += value; }
+        }
+
+        public Int32 Empates
+        {
+            get { return empates; }
+            set { empates += value; }
+        }
+
+        public void contratarJogador(Jogador J)
+        {
+            this.jogadores.Add(J);
+        }
+
+        private string demitirJogador(Jogador J)
+        {
+            int buscarJogador = this.jogadores.IndexOf(J);
+
+            string strRetorno = "Jogador" + J.getNome();
+            if (buscarJogador != -1)
+            {
+                this.jogadores.RemoveAt(buscarJogador);
+                return strRetorno + " demitido.";
+            }
+            return "Não foi possível demitir " + strRetorno;
+        }
+
+        private Jogador getJogador(int camisa)
+        {
+            return jogadores.Find(j => j.getCamisa().Equals(camisa));
+        }
+
+        public void gol(int camisa)
+        {
+            this.getJogador(camisa).Gols = 1;
+        }
+
+        public void getDescricaoJogadores()
+        {
+            foreach (Jogador jog in jogadores)
+            {
+                Console.WriteLine(jog.getDescricao());
+            }
+        }
+    }
+    
+}
